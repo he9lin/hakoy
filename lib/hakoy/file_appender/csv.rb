f715e2c1393e884d74752fb826efd0035f74bffd
Lin He <he9lin@gmail.com>
2014-09-06 19:17:26 -0700

    Fix duplicate checking

--- lib/hakoy/file_appender/csv.rb
+++ lib/hakoy/file_appender/csv.rb
@@ -8,15 +8,19 @@ module Hakoy
           def call(file_path, rows_hash, uid_key)
             results = []
 
-            check_duplidate = -> (row) do
-              rows_hash.each do |row_hash|
-                unless row[uid_key] == row_hash[uid_key]
-                  results << row_hash
-                end
+            rows_hash.each do |row_hash|
+              is_duplicate = false
+
+              CSV.foreach(file_path, headers: true) do |row|
+                row_uid_key = row[uid_key] || Hakoy::RowNormalizer::GenerateUniqueId.(row.to_hash)
+                is_duplicate = true if row_uid_key == row_hash[uid_key]
+              end
+
+              unless is_duplicate
+                results << row_hash
               end
             end
 
-            CSV.foreach(file_path, headers: true, &check_duplidate)
             results
           end
         end
@@ -32,26 +36,26 @@ module Hakoy
 
         return if rows_hash.empty?
 
-        CSV.open(file_path, 'a') do |to_file|
-          append_row_hash_values = -> (row_hash) do
-            append_to_csv_file(to_file, row_hash.values_at(*header_keys))
-          end
+        append_row_hash_values = -> (row_hash) do
+          append_to_csv_file(file_path, row_hash.values_at(*header_keys))
+        end
 
-          if file_exists
-            when_not_a_duplicate(file_path, rows_hash, uid_key, &append_row_hash_values)
-          else
-            # Add header for new file and no need to check duplicates
-            header_hash = rows_hash[0].keys.map {|key| keys_mapping.key(key) }
-            append_to_csv_file to_file, keys
-            rows_hash.each(&append_row_hash_values)
-          end
+        if file_exists
+          when_not_a_duplicate(file_path, rows_hash, uid_key, &append_row_hash_values)
+        else
+          # Add header for new file and no need to check duplicates
+          header_hash = rows_hash[0].keys.map {|key| keys_mapping.key(key) }
+          append_to_csv_file file_path, keys
+          rows_hash.each(&append_row_hash_values)
         end
       end
 
       private
 
-      def append_to_csv_file(to_file, *rows)
-        rows.each {|r| to_file << r}
+      def append_to_csv_file(file_path, *rows)
+        CSV.open(file_path, 'a') do |to_file|
+          rows.each {|r| to_file << r}
+        end
       end
 
       def when_not_a_duplicate(file_path, rows_hash, uid_key, &block)
